13697a6b04bec01f3d549d807ac7836ef2f07659
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2020-03-20 16:46:48 -0300

    ajustado webpack para funcionar problema como caminho output

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
   output: {
-    path: path.resolve('./main.js'),
     filename: 'main.js',
+    path: path.resolve(__dirname,'./'),
   },
 };
