a04b51fd4f9bdd901762fb5c094898c430a7edb9
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2020-03-23 14:56:28 -0300

    accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,46 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
+// export default function initAccordion() {
+//   const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+//   const activeClass = 'ativo';
 
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+//   function activeAccordion() {
+//     this.classList.toggle(activeClass);
+//     this.nextElementSibling.classList.toggle(activeClass);
+//   }
+
+//   if (accordionList.length) {
+//     accordionList[0].classList.add(activeClass);
+//     accordionList[0].nextElementSibling.classList.add(activeClass);
+
+//     accordionList.forEach((item) => {
+//       item.addEventListener('click', activeAccordion);
+//     });
+//   }
+// }
+
+export default class accordion {
+  constructor(list){
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+    debugger;
+  }
+  //adiciona os eventos ao accordion
+  addAccordionEvent(){
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
+
+  init(){
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent()
+    }
+  }
+
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  watch: true,
   entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
   output: {
     filename: 'main.js',
